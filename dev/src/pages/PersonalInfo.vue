72c7307fb517d2340889dd8cf9a5b1923554fdbe
s3930650 <s3930650@student.rmit.edu.au>
2022-09-18 15:04:52 +1000

    Responsive design changes

--- dev/src/pages/PersonalInfo.vue
+++ dev/src/pages/PersonalInfo.vue
@@ -4,11 +4,11 @@ import Navigation from '../components/Navigation.vue';
 
 <template>
     <Navigation />
-    <div class="flex flex-col md:flex-row items-center w-full bg-inherit md:bg-blue-400 lg:bg-inherit mt-12 space-y-4 md:space-y-0 px-4 mx-auto max-w-6xl">
+    <div class="flex flex-col md:flex-row items-center w-full bg-inherit lg:bg-inherit mt-12 space-y-4 md:space-y-0 px-4 mx-auto max-w-6xl">
         <div class="w-96">
             <img  class="w-full h-full rounded-full object-cover" id="portrait" alt="Picture of Leigh" title="Hello there" src="../assets/profile.jpg" />
         </div>
-        <div class="rounded-2xl w-3/4 text-sm flex flex-col justify-center px-12 space-y-2 py-4 ml-8 bg-white shadow-xl">
+        <div class="rounded-2xl w-11/12 md:w-3/4 text-sm flex flex-col justify-center px-4 md:px-12 space-y-2 py-4 md:ml-8 bg-white shadow-xl">
             <p>Name: <span class="font-semibold">Leigh White</span></p>
             <p>Student Number: <span class="font-semibold">S3930650</span></p>
             <p>Student Email: <a href="mailto:s3930650@student.rmit.edu.au" class="font-semibold hover:underline">s3930650@student.rmit.edu.au</a></p>
